cf1581e944420c54364a72c56f0f8da796c5444d
ilya-uzun <ilya.uzun@bk.ru>
2022-09-01 22:45:06 +0500

    Интеграция с СИ

---
+++ LibC/foo/foo.h
@@ -0,0 +1,6 @@
+#ifdef FOO_LIB
+#define FOO_LIB
+
+int foo(char* str);
+
+#endif
---
+++ LibC/foo/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+#include "foo.h"
+
+int main(int argc, char* argv[]){
+	char* str = "0123";
+	printf("%d\n", foo(str));
+}
