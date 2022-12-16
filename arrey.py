6e0584d9598dd7337ff04decd4ad65919705309b
ilya-uzun <ilya.uzun@bk.ru>
2022-12-16 21:36:12 +0500

    1

---
+++ arrey.py
@@ -0,0 +1,14 @@
+# Создаем пустой массив
+arr = []
+for i in range(1,18):
+    if (i % 15 == 0):
+        arr.append('FizzBuzz')
+    elif i % 5 == 0:
+        arr.append('Fizz')
+    elif i % 3 == 0:
+        arr.append('Buzz')
+    else:
+        arr.append(i)
+
+for i in arr:
+    print(i)
