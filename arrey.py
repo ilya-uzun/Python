4d02e08317e67b129567aac9f2fd6f7a53d97748
ilya-uzun <ilya.uzun@bk.ru>
2022-12-16 22:54:50 +0500

    arrey

--- arrey.py
+++ arrey.py
@@ -1,4 +1,7 @@
-# Создаем пустой массив
+from array import * # импорт класса array
+
+
+# Создаем пустой список
 arr = []
 for i in range(1,18):
     if (i % 15 == 0):
@@ -12,3 +15,23 @@ for i in range(1,18):
 
 for i in arr:
     print(i)
+
+# range - генерирует числа от 1 до 17 для цикла
+# arr.append - добавит элемент в конец списка
+
+
+arr2 = array('i', []) # Создаем пустой массив натуральных чисел
+
+for i in range(1,18):
+    if (i % 15 == 0):
+        arr2.append(0)
+    elif i % 5 == 0:
+        arr2.append(1)
+    elif i % 3 == 0:
+        arr2.append(2)
+    else:
+        arr2.append(i)
+
+for i in arr2:
+    print(i)
+
